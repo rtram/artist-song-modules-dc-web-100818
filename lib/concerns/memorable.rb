eeeacebd3b1ecda2db562ee701e5f47183ee01d1
Robin Tram <robin.tram@gmail.com>
2018-10-14 18:05:29 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,12 @@
 
 module Memorable
-
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
 
 end 
